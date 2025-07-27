6cd126ff5510c6947d67c08e3854fa830951d781
yaminisoni00 <yaminisoni00@gmail.com>
2025-07-27 21:02:20 +0530

    test

--- backend/src/services/activityLogs.service.ts
+++ backend/src/services/activityLogs.service.ts
@@ -8,10 +8,10 @@ export class ActivityLogsService {
 
     getActivityLogs = async (userDetails: any): Promise<any> => {
         try {
-            if (![USER_ROLE.ADMIN, USER_ROLE.MANAGER].includes(userDetails?.role)) {
-                return { message: ERROR_TYPES.ACCESS_DENIED.message };
-            }
-            const activityLogs = await ActivityLogModel.find({ userId: userDetails._id }).sort({ timestamp: -1 });
+            // if (![USER_ROLE.ADMIN, USER_ROLE.MANAGER].includes(userDetails?.role)) {
+            //     return { message: ERROR_TYPES.ACCESS_DENIED.message };
+            // }
+            const activityLogs = await ActivityLogModel.find().sort({ timestamp: -1 });
             return { message: SUCCESS_TYPES.DEFAULT_SUCCESS.message, activityLogs: activityLogs || [] };
         } catch (error) {
             console.log("Error getting activity logs", error);
