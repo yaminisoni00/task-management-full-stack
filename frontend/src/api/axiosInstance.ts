2fda94d0c0559385d61ad24c838e277b575daf2d
yaminisoni00 <yaminisoni00@gmail.com>
2025-07-27 20:05:13 +0530

    initial commit

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAuth } from 'firebase/auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   }, // adjust to your backend URL
